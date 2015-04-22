8674e022313a6d5786dc6fc20394427c0748e884
Victor Lecomte <victor.lecomte@student.uclouvain.be>
2015-04-22 22:07:58 +0200

    Implemented the factor list structure

---
+++ code/factor_list.c
@@ -0,0 +1,72 @@
+#include "factor_list.h"
+#include <stdlib.h>
+
+#define STARTING_SIZE 32
+
+factor_list_t *new_list()
+{
+    // Allocate the structure
+    factor_list_t *list = (factor_list_t *) malloc(sizeof(factor_list_t));
+    if (list == NULL)
+        exit(EXIT_FAILURE);
+    
+    // Start the list with a reasonable initial size
+    list->cont_size = STARTING_SIZE;
+    list->list = (factor_t *) malloc(list->cont_size * sizeof(factor_t));
+    if (list->list == NULL)
+        exit(EXIT_FAILURE);
+    
+    // The list is initially empty
+    list->size = 0;
+    
+    return list;
+}
+
+void free_list(factor_list_t *list)
+{
+    // Free the internal list then the structure
+    free(list->list);
+    free(list);
+}
+
+void push(factor_list_t *list, factor_t factor)
+{
+    // If there is not enough space
+    if (list->size == list->cont_size)
+    {
+        // Double the size
+        list->cont_size = 2 * list->size;
+        
+        // Copy the data
+        factor_t *copy = (factor_t *) malloc(list->cont_size * sizeof(factor_t));
+        for (int i = 0; i < list->cont_size; i++)
+            copy[i] = list->list[i];
+        
+        // Replace the internal list with the copy
+        free(list->list);
+        list->list = copy;
+    }
+    
+    // Add the factor at the end and increase the size
+    *end(list) = factor;
+    list->size++;
+}
+
+void remove(factor_list_t *list, factor_t *to_remove)
+{
+    // Decrease the size TODO test
+    list->size--;
+    
+    // Move the last element over to the free position
+    *to_remove = *end(list);
+}
+
+factor_t *begin(factor_list_t *list)
+{
+    return list->list;
+}
+
+factor_t *end(factor_list_t *list)
+{
+    return list->list + list->size;
+}
--- code/factor_list.h
+++ code/factor_list.h
@@ -1,8 +1,12 @@
+#ifndef _FACTOR_LIST_H
+#define _FACTOR_LIST_H
+
 typedef long long ll;
 
 typedef struct
 {
     ll num;
+    int occur;
     char *file;
 }
 factor_t;
@@ -17,7 +21,9 @@ factor_list_t;
 
 factor_list_t *new_list();
 void free_list(factor_list_t *list);
-void push(factor_list_t *list, factor_t fact);
-void remove(factor_list_t *list, *factor_t to_remove);
+void push(factor_list_t *list, factor_t factor);
+void remove(factor_list_t *list, factor_t *to_remove);
 factor_t *begin(factor_list_t *list);
 factor_t *end(factor_list_t *list);
+
+#endif
