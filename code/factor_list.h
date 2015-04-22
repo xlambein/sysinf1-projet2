0459bc906fd2936e4ea73dbcbc40d0f1a9c20653
Victor Lecomte <victor.lecomte@student.uclouvain.be>
2015-04-22 19:28:01 +0200

    Suggestion for the list format (both prime and waiting)

---
+++ code/factor_list.h
@@ -0,0 +1,22 @@
+typedef long long ll;
+
+typedef struct
+{
+    ll num;
+    char *file;
+}
+factor_t;
+
+typedef struct
+{
+    factor_t *list;
+    int size;
+    int cont_size;
+}
+factor_list_t;
+
+factor_list_t *new_list();
+void push(factor_list_t *list, factor_t fact);
+void remove(factor_list_t *list, *factor_t to_remove);
+factor_t *begin(factor_list_t *list);
+factor_t *end(factor_list_t *list);
