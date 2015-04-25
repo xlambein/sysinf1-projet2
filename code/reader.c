bc47757e05065928732d5aa9c22abde99ec83026
xlambein <xavier.lambein@student.uclouvain.be>
2015-04-25 14:13:01 +0200

    finished reader thread function

--- code/reader.c
+++ code/reader.c
@@ -16,7 +16,7 @@ void *reader(void *arg)
 
     check(!pthread_mutex_lock(&mut_state),
             "pthread_mutex_lock");
-    //TODO: increment reader counter
+    reader_count++;
     check(!pthread_mutex_unlock(&mut_state),
             "pthread_mutex_unlock");
     
@@ -28,7 +28,24 @@ void *reader(void *arg)
 
         check(!pthread_mutex_lock(&mut_state),
                 "pthread_mutex_lock");
-        //TODO: Do something with the number
+
+        // Divide the number by every factor in the factor list
+        for (factor_t * it = list_begin(waiting_list);
+                it != list_end(waiting_list);
+                ++it)
+        {
+            if ((number % it->num) == 0)
+                number /= it->num;
+        }
+
+        // Add it to the waiting list if it's not 1
+        if (number != 1)
+        {
+            factor_t tmp = {number, 1, st->filename};
+            list_push(waiting_list, tmp);
+            sem_post(&sem_full);
+        }
+
         check(!pthread_mutex_unlock(&mut_state),
                 "pthread_mutex_unlock");
         
@@ -36,15 +53,40 @@ void *reader(void *arg)
                 "sem_post");
     }
 
+error:
     check(!pthread_mutex_lock(&mut_state),
             "pthread_mutex_lock");
-    //TODO: decrement reader counter
+    reader_count--;
+    
+    // If this is the last reader
+    if (reader_count == 0)
+    {
+        find_prime_with_one_occurrence();
+        list_free(factor_list);
+    }
+    
     check(!pthread_mutex_unlock(&mut_state),
             "pthread_mutex_unlock");
 
-error:
+    if (st->stream != stdin)
+        fclose(st->stream);
+    free((void *) st->filename);
     free(st);
 
     return NULL;
+}
+
+void find_prime_with_one_occurrence()
+{
+    for (int i = 0; i < factor_list->size; i++)
+    {
+        if (factor_list->list[i].occur == 1)
+        {
+            found = true;
+            to_fact.num = 1; // This will stop the factorizer threads
+            finish(&factor_list->list[i]);
+            break;
+        }
+    }
 }
 
--- code/reader.h
+++ code/reader.h
@@ -3,14 +3,19 @@
 
 #include <stdio.h>
 
+#include "factor_list.h"
+
 typedef struct
 {
     FILE * stream;
+    const char * filename;
     //TODO: the rest
 }
 reader_starting_state_t;
 
 void *reader(void *arg);
+
+void find_prime_with_one_occurrence();
 
 #endif
 
