489e3bbb9a9b8d715d658a2567df7b54558ad6ff
xlambein <xavier.lambein@student.uclouvain.be>
2015-04-26 21:29:54 +0200

    added comments to curl_getter.*

--- code/curl_getter.h
+++ code/curl_getter.h
@@ -1,17 +1,26 @@
 #ifndef __curl_getter_h__
 #define __curl_getter_h__
 
+/**
+ * This file defines functions used to perform HTTP GET requests in a thread
+ * and write the response into a file descriptor.
+ */
+
 #include <stdlib.h>
 
+// Structure used to pass arguments to the thread function curl_getter()
 typedef struct
 {
-    int fd;
-    const char * url;
+    int fd; // A file descriptor
+    const char * url; // An url
 }
 curl_getter_starting_state_t;
 
+// Curl callback function, writes a buffer into a file descriptor
 size_t write_callback(void *buffer, size_t size, size_t nmemb, void *userp);
 
+// Thread function that GETs a file from an URL and writes it in a file
+// descriptor
 void *curl_getter(void *arg);
 
 #endif
