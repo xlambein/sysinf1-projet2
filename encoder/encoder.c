23c0a84f3d12acb6347c5f185ba65708e073c88c
Victor Lecomte <victor.lecomte@student.uclouvain.be>
2015-05-05 22:07:12 +0200

    Added a simple test case encoder

---
+++ encoder/encoder.c
@@ -0,0 +1,13 @@
+#include <endian.h>
+#include <inttypes.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    uint64_t number;
+    while (scanf("%" PRId64, &number) == 1)
+    {
+        number = htobe64(number);
+        fwrite(&number, sizeof(uint64_t), 1, stdout);
+    }
+}
